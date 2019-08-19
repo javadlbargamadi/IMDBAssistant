2315944478f203833c8cf89b9faa574dcf7daefb
Javad Bargamadi <javadbargamadi1375@gmail.com>
2019-08-19 22:34:53 +0430

    async

--- app/src/main/java/com/sematecjavaproject/imdbassistant/MainActivity.java
+++ app/src/main/java/com/sematecjavaproject/imdbassistant/MainActivity.java
@@ -1,8 +1,12 @@
 package com.sematecjavaproject.imdbassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText edtEnterTitle = findViewById(R.id.edtEnterTitle);
+        Button btnSearchTitle = findViewById(R.id.btnSearchTitle);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        btnSearchTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userMovie;
+                userMovie = edtEnterTitle.getText().toString();
+            }
+        });
     }
 }
