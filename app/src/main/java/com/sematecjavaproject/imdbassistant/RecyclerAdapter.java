01b7d0a6fd98875bde330d76e36b4cab47bb6976
Javad Bargamadi <javadbargamadi1375@gmail.com>
2019-08-20 00:21:58 +0430

    50% adapter

--- app/src/main/java/com/sematecjavaproject/imdbassistant/RecyclerAdapter.java
+++ app/src/main/java/com/sematecjavaproject/imdbassistant/RecyclerAdapter.java
@@ -1,17 +1,34 @@
 package com.sematecjavaproject.imdbassistant;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.sematecjavaproject.imdbassistant.IMDBClass.Search;
+
+import java.util.List;
+
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder> {
+
+    List<Search> searchResult;
+
+    public RecyclerAdapter(List<Search> searchList) {
+
+        searchResult = searchList;
+    }
 
     @NonNull
     @Override
     public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View viewItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item1, parent, false);
+        RecyclerViewHolder viewHolder = new RecyclerViewHolder(viewItem);
+        return viewHolder;
     }
 
     @Override
@@ -21,14 +38,19 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Recycl
 
     @Override
     public int getItemCount() {
-        return 0;
+        return searchResult.size();
     }
 
     class RecyclerViewHolder extends RecyclerView.ViewHolder {
 
+        ImageView imageViewPoster;
+        TextView textViewTitle;
+        TextView textViewRate;
 
         public RecyclerViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            imageViewPoster =itemView.findViewById(R.id.txtTitle);
         }
     }
 }
